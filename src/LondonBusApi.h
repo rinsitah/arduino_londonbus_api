25ad1f10011af65de31b409d452e50ad52419e05
rinsitah <35811640+rinsitah@users.noreply.github.com>
2018-01-30 10:54:15 +0000

    Update LondonBusApi.h

--- src/LondonBusApi.h
+++ src/LondonBusApi.h
@@ -29,7 +29,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 struct TFLTickerResponse{
     
   String lineName;
-  String platformMame;
+  String platformName;
   double timeToStation;
   String towards;
   String expectedArrival;
